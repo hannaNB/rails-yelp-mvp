5834adbe1f55daca8e0ff2aac6e28a204a4c18dc
Hanna Nabet <hanna.nabet@yahoo.fr>
2019-12-15 16:19:41 +0100

    CSS to finish

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
     validates :name, uniqueness: true, presence: true, length: { minimum: 2 }
     validates :address, presence: true
     validates :phone_number, presence: true
-    validates :category, presence: true
+    validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false}
     has_many :reviews, dependent: :destroy
 end
